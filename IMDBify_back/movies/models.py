a402480af0ec05351b76cc678d9995c7096cde8f
Tuhin Roy <troy0870@gmail.com>
2020-10-06 14:15:15 +0530

    add model for movie

--- IMDBify_back/movies/models.py
+++ IMDBify_back/movies/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+class Movie(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, default='')
+    image = models.CharField(max_length=100, default=''),
+    year_of_release = models.IntegerField(),
+    director = models.CharField(max_length=50, default='')
+
+    class Meta:
+        ordering = ['created']
+
