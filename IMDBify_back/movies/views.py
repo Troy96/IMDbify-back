aa26d7dc8c7a9ddbc6225d643336e751f86aa1d8
Tuhin Roy <troy0870@gmail.com>
2020-10-06 20:37:11 +0530

    fix  scraper wiring issue

--- IMDBify_back/movies/views.py
+++ IMDBify_back/movies/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics, filters
+from django.http import HttpResponse
+from rest_framework.parsers import JSONParser
 from movies.models import Movie
 from movies.serializers import MoviesSerializer
+from .scraper import scrape
+import io
 
 class MovieList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
@@ -15,4 +19,15 @@ class MovieDetail(generics.RetrieveAPIView):
 
 def scrape_data(request):
     url_to_scrap = request.GET.get('url', '')
+    data = scrape(url_to_scrap)
+    for index in range(0, len(data)):
+        scraped_movie = data[index]
+        movie = Movie()
+        movie.name = scraped_movie['name']
+        movie.image = scraped_movie['image']
+        movie.year_of_release = scraped_movie['year_of_release']
+        movie.director = scraped_movie['director']
+
+        movie.save()
+    return HttpResponse(status=201)
 
