2e7965431842dc78971eaeb572260b77fd22e0f8
Tuhin Roy <troy0870@gmail.com>
2020-10-06 16:09:59 +0530

    add search functionality on MovieList class

--- IMDBify_back/movies/views.py
+++ IMDBify_back/movies/views.py
@@ -1,12 +1,12 @@
-from django.http import JsonResponse, HttpResponse
-from rest_framework.parsers import JSONParser
-from rest_framework import generics
+from rest_framework import generics, filters
 from movies.models import Movie
 from movies.serializers import MoviesSerializer
 
 class MovieList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MoviesSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'year_of_release']
 
 
 class MovieDetail(generics.RetrieveAPIView):
