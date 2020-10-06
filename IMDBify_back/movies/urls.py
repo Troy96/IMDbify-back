b2be211080545afb0a2da0113fe4710bc6dbc3fa
Tuhin Roy <troy0870@gmail.com>
2020-10-06 20:37:06 +0530

    fix  scraper wiring issue

--- IMDBify_back/movies/urls.py
+++ IMDBify_back/movies/urls.py
@@ -3,5 +3,6 @@ from movies import views
 
 urlpatterns = [
     path('', views.MovieList.as_view()),
-    path('<int:pk>', views.MovieDetail.as_view())
+    path('<int:pk>', views.MovieDetail.as_view()),
+    path('scrape', views.scrape_data)
 ]
